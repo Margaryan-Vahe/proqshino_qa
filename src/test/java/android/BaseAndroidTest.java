f8ee40e11d86991c2ccae763ac3fa76dac4c9d5a
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-08-07 08:32:28 +0400

    add: RecoverPageAndroid in BaseAndroidTest

--- src/test/java/android/BaseAndroidTest.java
+++ src/test/java/android/BaseAndroidTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import pages.android.InputPinPageAndroid;
 import pages.android.LoginPageAndroid;
 import pages.android.MainPageAndroid;
+import pages.android.RecoverPageAndroid;
 
 import static baseUtils.SetupCapabilities.appiumDriver;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -14,6 +15,7 @@ public class BaseAndroidTest{
     MainPageAndroid mainPageAndroid;
     LoginPageAndroid loginPageAndroid;
     InputPinPageAndroid inputPinPageAndroid;
+    RecoverPageAndroid recoverPageAndroid;
 
     @BeforeEach
     public void initPages() throws Exception {
@@ -23,6 +25,7 @@ public class BaseAndroidTest{
         mainPageAndroid = new MainPageAndroid(appiumDriver);
         loginPageAndroid = new LoginPageAndroid(appiumDriver);
         inputPinPageAndroid = new InputPinPageAndroid(appiumDriver);
+        recoverPageAndroid = new RecoverPageAndroid(appiumDriver);
     }
 
     @AfterEach
