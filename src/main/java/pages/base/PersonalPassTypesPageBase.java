18106eac55527cc37096f5d9bd32de953ce798ae
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-08-14 17:01:39 +0400

    add: PersonalPassTypesPage classes

---
+++ src/main/java/pages/base/PersonalPassTypesPageBase.java
@@ -0,0 +1,39 @@
+package pages.base;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.qameta.allure.Step;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+
+public abstract class PersonalPassTypesPageBase {
+    protected final AppiumDriver<MobileElement> appiumDriver;
+
+    // Локаторы элементов страницы
+    public abstract SelenideElement pageHeader();
+    public abstract SelenideElement qrPass();
+
+
+    // Конструктор класса
+    public PersonalPassTypesPageBase(AppiumDriver<MobileElement> appiumDriver) {
+        this.appiumDriver = appiumDriver;
+    }
+
+    // Методы класса
+    @Step("Жду загрузки экрана 'Выберите тип пропуска'")
+    public PersonalPassTypesPageBase waitUntilLoaded() {
+        pageHeader().shouldBe(visible, Duration.ofSeconds(10));
+        return this;
+    }
+
+    @Step("Перехожу в раздел 'Личные пропуска'")
+    public void clickToQRPassButton(){
+        qrPass()
+                .shouldBe(visible, Duration.ofSeconds(10))
+                .click();
+    }
+
+}
