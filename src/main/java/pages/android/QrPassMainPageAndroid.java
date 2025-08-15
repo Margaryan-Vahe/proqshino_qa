ece8b4af9548f3cc61ace1ea6845d3a7324fcab7
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-08-15 14:01:44 +0400

    add: QrPassMainPages

---
+++ src/main/java/pages/android/QrPassMainPageAndroid.java
@@ -0,0 +1,56 @@
+package pages.android;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import pages.base.QrPassMainPageBase;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class QrPassMainPageAndroid extends QrPassMainPageBase {
+    // Элементы страницы
+
+    @Override
+    public SelenideElement pageHeader() {
+        return $(MobileBy.AccessibilityId("QR"));
+    }
+
+    @Override
+    public SelenideElement alreadyHavePassText() {
+        return $(MobileBy.AccessibilityId("У вас уже есть пропуск QR"));
+    }
+
+    @Override
+    public SelenideElement generateButton() {
+        return $(MobileBy.AccessibilityId("Сгенерировать"));
+    }
+
+    @Override
+    public SelenideElement modalWindowSuccessApproved() {
+        return $(MobileBy.xpath("//android.view.View[contains(@content-desc, 'Пропуск сформирован')]"));
+    }
+
+    @Override
+    public SelenideElement modalWindowSuccessNotApproved() {
+        return $(MobileBy.AccessibilityId("Для вас согласован доступ в БК \"Прокшино\". Пожалуйста, предъявите паспорт на ресепшен для подтверждения личности."));
+    }
+
+    @Override
+    public SelenideElement confirmRadioButton() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.view.View\").instance(6)"));
+    }
+
+    @Override
+    public SelenideElement myPassesButton() {
+        return $(MobileBy.AccessibilityId("Мои пропуска"));
+    }
+
+    // Конструктор класса
+    public QrPassMainPageAndroid(AppiumDriver<MobileElement> appiumDriver) {
+        super(appiumDriver);
+    }
+
+    // Методы класса
+
+}
---
+++ src/main/java/pages/base/QrPassMainPageBase.java
@@ -0,0 +1,77 @@
+package pages.base;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.qameta.allure.Step;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+
+public abstract class QrPassMainPageBase {
+    protected final AppiumDriver<MobileElement> appiumDriver;
+
+
+
+    // Локаторы элементов страницы
+    public abstract SelenideElement pageHeader();
+    public abstract SelenideElement alreadyHavePassText();
+    public abstract SelenideElement generateButton();
+    public abstract SelenideElement modalWindowSuccessApproved();
+    public abstract SelenideElement modalWindowSuccessNotApproved();
+    public abstract SelenideElement confirmRadioButton();
+    public abstract SelenideElement myPassesButton();
+    // Конструктор класса
+    public QrPassMainPageBase(AppiumDriver<MobileElement> appiumDriver) {
+        this.appiumDriver = appiumDriver;
+    }
+
+    // Методы класса
+    @Step("Жду загрузки экрана 'QR'")
+    public QrPassMainPageBase waitUntilLoaded() {
+        pageHeader().shouldBe(visible, Duration.ofSeconds(10));
+        return this;
+    }
+    @Step("Жду загрузки экрана 'QR': У вас уже есть новый пропуск")
+    public void waitUntilLoadedAlreadyHavePassText() {
+        alreadyHavePassText().shouldBe(visible, Duration.ofSeconds(10));
+    }
+    @Step("Жду загрузки экрана 'Успешно' (без подтверждения на ресепшене)")
+    public void waitUntilLoadedModalWindowSuccessApproved() {
+        modalWindowSuccessApproved().shouldBe(visible, Duration.ofSeconds(10));
+    }
+    @Step("Жду загрузки экрана 'Успешно' (без подтверждения на ресепшене)")
+    public void waitUntilLoadedModalWindowSuccessNotApproved() {
+        modalWindowSuccessNotApproved().shouldBe(visible, Duration.ofSeconds(10));
+    }
+    @Step("Нажимаю на кнопку 'Сгенерировать'")
+    public void clickToGenerateButton() {
+        generateButton()
+                .shouldBe(visible, Duration.ofSeconds(10))
+                .click();
+    }
+    @Step("Нажимаю на радио-баттон 'Я подойду на ресепшен с паспортом'")
+    public void clickToRadioButton() {
+        confirmRadioButton().shouldBe(visible, Duration.ofSeconds(10));
+    }
+    @Step("Нажимаю на кнопку 'Мои пропуска'")
+    public void clickToMyPassesButton() {
+        myPassesButton()
+                .shouldBe(visible, Duration.ofSeconds(10))
+                .click();
+    }
+
+    @Step("Подаю заявку на личный QR-пропуск: подтвержден")
+    public void requestPersonalQRPassBeingApproved() {
+        waitUntilLoaded();
+        clickToGenerateButton();
+        waitUntilLoadedModalWindowSuccessApproved();
+        clickToMyPassesButton();
+    }
+    @Step("Подаю заявку на личный QR-пропуск: не подтвержден")
+    public void requestPersonalQRPassBeingNotApproved() {
+        myPassesButton().shouldBe(visible, Duration.ofSeconds(10));
+    }
+
+}
