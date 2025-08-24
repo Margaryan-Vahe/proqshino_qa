00231ea4f476c2f4b36069a6698676131c93c12a
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-08-24 22:34:04 +0400

    add: APPROVED_USER data

--- src/main/java/baseUtils/Data.java
+++ src/main/java/baseUtils/Data.java
@@ -6,9 +6,11 @@ public class Data {
             public String phoneValidValue() {
                 return "79330000000";
             }
+
             public String phoneFullValue() {
                 return "+7 (933) 000-00-00";
             }
+
             public String phoneInvalidValue() {
                 return "793300000";
             }
@@ -16,13 +18,14 @@ public class Data {
             public String passwordValidValue() {
                 return "Password1";
             }
+
             public String passwordInvalidValue() {
                 return "Password123";
             }
+
             public String passwordIncorrectValue() {
                 return "Password";
             }
-
 
             public String userFirstName() {
                 return "USER";
@@ -44,9 +47,11 @@ public class Data {
             public String phoneValidValue() {
                 return "79330000001";
             }
+
             public String phoneFullValue() {
                 return "+7 (933) 000-00-01";
             }
+
             public String phoneInvalidValue() {
                 return "793300000";
             }
@@ -54,13 +59,14 @@ public class Data {
             public String passwordValidValue() {
                 return "Password1";
             }
+
             public String passwordInvalidValue() {
                 return "Password123";
             }
+
             public String passwordIncorrectValue() {
                 return "Password";
             }
-
 
             public String userFirstName() {
                 return "USER";
@@ -77,39 +83,90 @@ public class Data {
             public String userEmail() {
                 return "9330000001@test.com";
             }
+        },
+        APPROVED_USER {
+            public String phoneValidValue() {
+                return "79330000002";
+            }
+
+            public String phoneFullValue() {
+                return "+7 (933) 000-00-02";
+            }
+
+            public String phoneInvalidValue() {
+                return "793300000";
+            }
+
+            public String passwordValidValue() {
+                return "Password1";
+            }
+
+            public String passwordInvalidValue() {
+                return "Password123";
+            }
+
+            public String passwordIncorrectValue() {
+                return "Password";
+            }
+
+            public String userFirstName() {
+                return "USER";
+            }
+
+            public String userSecondName() {
+                return "TEST";
+            }
+
+            public String userLastName() {
+                return "APPROVED";
+            }
+
+            public String userEmail() {
+                return "9330000002@test.com";
+            }
         };
 
         public String phoneValidValue() {
             return "";
         }
+
         public String phoneFullValue() {
             return "";
         }
+
         public String phoneInvalidValue() {
             return "";
         }
+
         public String passwordValidValue() {
             return "";
         }
+
         public String passwordInvalidValue() {
             return "";
         }
+
         public String passwordIncorrectValue() {
             return "";
         }
+
         public String userFirstName() {
             return "";
         }
+
         public String userSecondName() {
             return "";
         }
+
         public String userLastName() {
             return "";
         }
+
         public String userEmail() {
             return "";
         }
-        public String companyInn(){
+
+        public String companyInn() {
             return "";
         }
     }
