4b84f9ec2efe7a0a2fd0374b6a89e863edf1cf7f
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-09-01 16:21:03 +0400

    add: SetPersonalDataPage classes

---
+++ src/main/java/pages/android/SetPersonalDataPageAndroid.java
@@ -0,0 +1,73 @@
+package pages.android;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import pages.base.SetPersonalDataPageBase;
+
+import java.util.Set;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class SetPersonalDataPageAndroid extends SetPersonalDataPageBase {
+    @Override
+    public SelenideElement pageHeader() {
+        return $(MobileBy.AccessibilityId("Заполните данные"));
+    }
+
+    @Override
+    public SelenideElement employeeRadioButton() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.view.View\").instance(7)"));
+    }
+
+    @Override
+    public SelenideElement lastNameField() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(0)"));
+    }
+
+    @Override
+    public SelenideElement firstNameField() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(1)"));
+    }
+
+    @Override
+    public SelenideElement secondNameField() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(2)"));
+    }
+
+    @Override
+    public SelenideElement emailField() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.widget.EditText\").instance(3)"));
+    }
+
+    @Override
+    public SelenideElement innField() {
+        return $(MobileBy.xpath("//android.view.View[@content-desc='ИНН компании']/following-sibling::android.widget.EditText"));
+    }
+
+    @Override
+    public SelenideElement continueButton() {
+        return $(MobileBy.AccessibilityId("Продолжить"));
+    }
+
+    @Override
+    public SelenideElement regSuccessWindowNotApprovedCase() {
+        return $(MobileBy.xpath("//android.view.View[contains(@content-desc, 'подтверждения вас')]"));
+    }
+
+    @Override
+    public SelenideElement reviewedCheckBox() {
+        return $(MobileBy.AndroidUIAutomator("new UiSelector().className(\"android.view.View\").instance(6)"));
+    }
+
+    @Override
+    public SelenideElement toMainPageButton() {
+        return $(MobileBy.AccessibilityId("На главный экран"));
+    }
+
+    // Конструктор класса
+    public SetPersonalDataPageAndroid(AppiumDriver<MobileElement> appiumDriver) {
+        super(appiumDriver);
+    }
+}
---
+++ src/main/java/pages/base/SetPersonalDataPageBase.java
@@ -0,0 +1,121 @@
+package pages.base;
+
+import com.codeborne.selenide.SelenideElement;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.qameta.allure.Step;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+
+public abstract class SetPersonalDataPageBase {
+    protected final AppiumDriver<MobileElement> appiumDriver;
+
+    // Локаторы экрана
+    public abstract SelenideElement pageHeader();
+    public abstract SelenideElement employeeRadioButton();
+    public abstract SelenideElement lastNameField();
+    public abstract SelenideElement firstNameField();
+    public abstract SelenideElement secondNameField();
+    public abstract SelenideElement emailField();
+    public abstract SelenideElement innField();
+    public abstract SelenideElement continueButton();
+
+    public abstract SelenideElement regSuccessWindowNotApprovedCase();
+    public abstract SelenideElement reviewedCheckBox();
+    public abstract SelenideElement toMainPageButton();
+
+
+    public SetPersonalDataPageBase(AppiumDriver<MobileElement> appiumDriver) {
+        this.appiumDriver = appiumDriver;
+    }
+
+    @Step("Жду загрузки экрана 'Заполните данные'")
+    public SetPersonalDataPageBase waitUntilLoaded() {
+        pageHeader().shouldBe(visible, Duration.ofSeconds(10));
+        return this;
+    }
+    @Step("Жду загрузки модального окна 'Вы успешно зарегистрировались'")
+    public void waitUntilLoadedSuccessModalWindow() {
+        regSuccessWindowNotApprovedCase().shouldBe(visible, Duration.ofSeconds(10));
+    }
+    @Step("Нажимаю на радио-батон 'Я сотрудник компании'")
+    public void clickToRadioButton() {
+       waitUntilLoaded();
+       employeeRadioButton().click();
+    }
+    @Step("Ввожу данные в поле 'Фамилия'")
+    public void typeLastName(String lastName) {
+        waitUntilLoaded();
+        SelenideElement lastName_ = lastNameField();
+        lastName_.shouldBe(visible).click();
+        lastName_.sendKeys(lastName);
+        appiumDriver.hideKeyboard();
+    }
+    @Step("Ввожу данные в поле 'Имя'")
+    public void typeFirstName(String firstName) {
+        waitUntilLoaded();
+        SelenideElement firstName_ = firstNameField();
+        firstName_.shouldBe(visible).click();
+        firstName_.sendKeys(firstName);
+        appiumDriver.hideKeyboard();
+    }
+    @Step("Ввожу данные в поле 'Отчество'")
+    public void typeSecondName(String secondName) {
+        waitUntilLoaded();
+        SelenideElement secondName_ = secondNameField();
+        secondName_.shouldBe(visible).click();
+        secondName_.sendKeys(secondName);
+        appiumDriver.hideKeyboard();
+    }
+    @Step("Ввожу данные в поле 'Email'")
+    public void typeEmail(String email) {
+        waitUntilLoaded();
+        SelenideElement email_ = emailField();
+        email_.shouldBe(visible).click();
+        email_.sendKeys(email);
+        appiumDriver.hideKeyboard();
+    }
+    @Step("Ввожу данные в поле 'ИНН'")
+    public void typeInn(String inn) {
+        SelenideElement inn_ = innField();
+        inn_.shouldBe(visible).click();
+        inn_.sendKeys(inn);
+        appiumDriver.hideKeyboard();
+    }
+    @Step("Активирую чек-бокс 'Я ознакомился с указанной информацией'")
+    public void clickToCheckBox() {
+        waitUntilLoadedSuccessModalWindow();
+        reviewedCheckBox()
+                .shouldBe(visible)
+                .click();
+    }
+    @Step("Нажимаю на кнпоку 'На главный экран'")
+    public void clickToMainPageButton() {
+        waitUntilLoadedSuccessModalWindow();
+        toMainPageButton()
+                .shouldBe(visible)
+                .click();
+    }
+    @Step("Успешно ввожу все данные")
+    public void typeAllData(
+            String lastName,
+            String firstName,
+            String secondName,
+            String email,
+            String inn) throws InterruptedException {
+        clickToRadioButton();
+
+        typeLastName(lastName);
+        typeFirstName(firstName);
+        typeSecondName(secondName);
+        typeEmail(email);
+        typeInn(inn);
+
+        Thread.sleep(1500);
+        continueButton().click();
+        clickToCheckBox();
+        clickToMainPageButton();
+    }
+}
