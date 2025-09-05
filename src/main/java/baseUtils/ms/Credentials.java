07e6ba0da196e541dc2c67da354651c6d636bee7
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-09-05 14:21:08 +0400

    new: Credentials class

---
+++ src/main/java/baseUtils/ms/Credentials.java
@@ -0,0 +1,13 @@
+package baseUtils.ms;
+
+public class Credentials {
+    public String password;
+    public String rememberMe;
+    public String username;
+
+    public Credentials(String password, String rememberMe, String username) {
+        this.password = password;
+        this.rememberMe = rememberMe;
+        this.username = username;
+    }
+}
