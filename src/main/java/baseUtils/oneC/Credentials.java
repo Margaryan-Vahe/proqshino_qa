9f3643728a99afa4baa4554f49aba8a6aa2233b1
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-09-14 21:20:27 +0400

    new: baseUtils.oneC.Credentials

---
+++ src/main/java/baseUtils/oneC/Credentials.java
@@ -0,0 +1,11 @@
+package baseUtils.oneC;
+
+public class Credentials {
+    public String username;
+    public String password;
+
+    public Credentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
