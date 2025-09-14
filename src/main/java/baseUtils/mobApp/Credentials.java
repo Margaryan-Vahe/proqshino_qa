95ad79262f1faf73ed55c45b8985e0e9e66e5999
Margaryan-Vah <margaryan.vk@yandex.ru>
2025-09-14 16:30:09 +0400

    add: baseUtils.mobApp.Credentials

---
+++ src/main/java/baseUtils/mobApp/Credentials.java
@@ -0,0 +1,11 @@
+package baseUtils.mobApp;
+
+public class Credentials {
+    public String phoneNumber;
+    public String password;
+
+    public Credentials(String phoneNumber, String password) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+}
